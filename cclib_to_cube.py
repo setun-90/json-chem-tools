32c8e2eccf109089fa00f53c16c2853527cd3f7f
Daniel Campos do Nascimento <dcdn.96@gmail.com>
2018-05-08 19:12:38 +0200

    Nettoyage du code et des commentaires

--- cclib_to_cube.py
+++ cclib_to_cube.py
@@ -4,22 +4,19 @@
 
 
 ## Usage
-from sys import argv, stderr
+from sys import argv
 if len(argv) < 5:
-	stderr.write("Usage: {} $job[=$value] $npts ${{input}}.log ${{output}}.cube\n".format(argv[0]))
+	from sys import stderr, exit
+	stderr.write("Usage: {} $job[=$value] $npts ${{input}}.log ${{output}}\n".format(argv[0]))
 	exit(1)
 
-from sys import stdin, stdout
 from math import pi, ceil
 from physics import A_to_a0
 from orbkit import grid, core
 
-## For Python 2 & 3 interoperability (`print` is a statement in Python 2)
-write = stdout.write
 
 
-
-## Set input/output
+## Input
 from cclib.parser import ccopen
 from orbkit.read import convert_cclib
 qc = convert_cclib(ccopen(argv[3]).parse(), all_mo=True)
@@ -27,10 +24,9 @@ qc = convert_cclib(ccopen(argv[3]).parse(), all_mo=True)
 
 
 ## Size: gaussian cube protocol
-over_s = 5   ## to be tuned
+over_s = 5   # to be tuned
 
-## (p|s).
-## Try to treat it as a number
+## Try to treat size parameter as a number
 try:
 	p_npts = int(argv[2])
 	## Two regimes: positive or zero, and negative
@@ -43,23 +39,17 @@ try:
 	## Negative: the spacing is given and the number of points is deduced
 	else:
 		## -1 is not implemented
-		#grid.adjust_to_geo(qc, over_s, 2.0**(2+p_npts)/3.0   if -5 < p_npts < -1 else \
-		#                              -p_npts*1e-3/A_to_a0  if p_npts <= -5 else None)
-
 		grid.delta_ = [2.0**(2+p_npts)/3.0   if -5 < p_npts < -1 else \
 		               -p_npts*1e-3/A_to_a0  if p_npts <= -5 else None]*3
 
 	del p_npts
 
-## Didn't work - it's a keyword
+## Didn't work - parameter is a keyword
 except ValueError:
-	#grid.adjust_to_geo(qc, over_s, 1.0/3.0  if argv[2] == "Coarse" else \
-	#                               1.0/6.0  if argv[2] == "Medium" else \
-	#                               1.0/12.0 if argv[2] == "Fine" else None)
-
 	grid.delta_ = [1.0/3.0  if argv[2] == "Coarse" else \
 	               1.0/6.0  if argv[2] == "Medium" else \
 	               1.0/12.0 if argv[2] == "Fine" else None]*3
+
 grid.max_ = list(map(lambda a: max(a) + over_s, qc.geo_spec))
 grid.min_ = list(map(lambda a: min(a) - over_s, qc.geo_spec))
 grid.init()
@@ -99,4 +89,4 @@ out = func(qc)
 
 ## Output
 from orbkit.output import main_output
-main_output(out[0,:], qc.geo_info, qc.geo_spec, outputname=argv[4], otype='cb')
+main_output(out[0,:], qc.geo_info, qc.geo_spec, outputname=argv[4], otype="cb")
