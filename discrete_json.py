223e14c53cd4d4ec0b4cad7f2fbc78ae2488624e
Daniel Campos do Nascimento <dcdn.96@gmail.com>
2018-05-14 21:48:13 +0200

    Préparations pour utiliser numpy

--- discrete_json.py
+++ discrete_json.py
@@ -11,7 +11,7 @@ if len(argv) < 3:
 	exit(1)
 
 from sys import stdin, stdout
-#from numpy import 
+from numpy import ndarray, linspace
 from json import load
 from physics import A_to_a0
 from wavefunction import primitives, psi_MO
@@ -66,10 +66,15 @@ except ValueError:
 	             (1.0/12.0,)*3 if argv[2] == "Fine" else None
 	p1, p2, p3 = [int(t_x/s1 + 1), int(t_y/s2 + 1), int(t_z/s3 + 1)]
 
+## Configure grid
+X, Y, Z = [x_min + s_x*n for n in range(p1)], [y_min + s_y*n for n in range(p2)], [z_min + s_z*n for n in range(p3)]
+#X, Y, Z = linspace(x_min, x_max, s_x), linspace(y_min, y_max, s_y), linspace(z_min, z_max, s_z)
+
 
 
 ## Temporarily work with redundant subshells (i.e. without S=P hybridization)
 temp_shell = [(i, ssh) for i, atom in enumerate(details["general"]["basis_set"], 1) for ssh in atom]
+#temp_shell = primitives(details["general"]["basis_set"])
 sh_to_l = {
 	u"S": 0,
 	u"P": 1,
@@ -79,7 +84,6 @@ sh_to_l = {
 	u"H": 5
 }
 shell_coefficients = map(list, zip(*[(sh_to_l[ssh[1][0]], tuple(orb)) for ssh in temp_shell for orb in ssh[1][1]]))
-# = primitives(details["general"]["basis_set"])
 
 
 
@@ -94,7 +98,7 @@ if job == "MO":
 		MO = [int(value)]
 	except ValueError:
 		if value == "All":
-			MO = [i for i in range(HOMO + 1)]
+			MO = range(HOMO + 1)
 		elif value == "Valence": pass
 		elif value == "Virtuals": pass
 		elif value in {"OccA","OccB"}: pass
@@ -128,14 +132,14 @@ write(" MO coefficients\n" if job == "MO" else
       " Electrostatic potential from Total SCF Density\n" if job == "Potential" and value == "SCF" else
       " Electron density from Total SCF Density\n" if job == "FDensity" and value == "SCF" else None)
 
-write(" {:> 4d}{:>12.6f}{:>12.6f}{:>12.6f}    {:d}\n".format(-n_atoms if job == "MO" else n_atoms, x_min, y_min, z_min, n_val))
+write("{:> 5d}{:>12.6f}{:>12.6f}{:>12.6f}{:>5d}\n".format(-n_atoms if job == "MO" else n_atoms, x_min, y_min, z_min, n_val))
 
-line = " {:> 4d}{:>12.6f}{:>12.6f}{:>12.6f}\n".format
+line = "{:> 5d}{:>12.6f}{:>12.6f}{:>12.6f}\n".format
 write(line(p1, s1, 0, 0))
 write(line(p2, 0, s2, 0))
 write(line(p3, 0, 0, s3))
 
-line = "  {:> 3d}{:>12.6f}{:>12.6f}{:>12.6f}{:>12.6f}\n".format
+line = "{:> 5d}{:>12.6f}{:>12.6f}{:>12.6f}{:>12.6f}\n".format
 for a, p in zip(molecule["atoms_Z"], molecule["starting_geometry"]):
 	write(line(a, a, p[0], p[1], p[2]))
 
@@ -143,7 +147,7 @@ for a, p in zip(molecule["atoms_Z"], molecule["starting_geometry"]):
 
 ## Specific to MO orbitals (c.f. cubegen documentation)
 if job == "MO":
-	write(" {:> 4d}".format(n_val))
+	write("{:> 5d}".format(n_val))
 	## TODO: check formatting in case of multiple orbitals
 	for o in MO:
 		write("  {:> 3d}".format(o))
@@ -154,8 +158,6 @@ if job == "MO":
 ## Generate voxel data
 l = p3*n_val
 block = ((" {: .5E}"*6 + "\n")*(l//6) + (" {: .5E}"*(l%6) + "\n" if l%6 > 0 else "")).format
-
-X, Y, Z = [x_min + s_x*n for n in range(p1)], [y_min + s_y*n for n in range(p2)], [z_min + s_z*n for n in range(p3)]
 
 
 
